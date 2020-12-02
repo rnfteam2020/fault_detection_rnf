19787af2f2858e54894d64c9257b3b805886b2f6
xvoron <artem_voronin95@mail.ru>
2020-12-02 20:53:17 +0100

    NN new class, requirements + progress

--- nn.py
+++ nn.py
@@ -2,7 +2,9 @@
 
 """
 import torch
+import torch.nn as nn
 import time
+from progress.bar import Bar
 
 class BaseNN(torch.nn.Module):
 
@@ -24,9 +26,33 @@ class BaseNN(torch.nn.Module):
         return name
 
 
+class Model(BaseNN):
+    def __init__(self, batch_size, input_dim, output_dim):
+        super().__init__()
+        N1 = 10
+        self.layer1 = nn.Linear(input_dim, N1, bias=False)
+        self.layer2 = nn.Linear(N1, output_dim, bias=False)
+        self.layer_output = nn.LogSoftmax(dim=1)
+
+    def forward(self, x):
+        x = nn.functional.relu(self.layer1(x))
+        x = self.layer2(x)
+        x = self.layer_output(x)
+        return x
 
 
+def fit(x_train, y_train, lr=0.001, epochs=1000, batch_size=32):
+    bar = Bar('Epochs', max=epochs)
+    model = Model(1,2,3)
 
+    loss_function = nn.MSELoss()
+    optimizer = torch.optim.Adam(model.parameters(), lr=lr)
+
+    for i in range(epochs):
+        bar.next()
+
+    bar.finish()
 
 if __name__ == "__main__":
-    pass
+    fit(1,2)
+
