d33a723fb8d01d5e6613890325a7b2580714fe5b
xvoron <artem_voronin95@mail.ru>
2020-12-05 15:30:41 +0100

    Data from pneumatic actuator

--- data.py
+++ data.py
@@ -2,14 +2,31 @@
 Import data
 
 For debug purpose sine wave is ok :)
+
+Possible import examples:
+-----------------------------------------------------------
+    import numpy as np
+    import h5py
+    f = h5py.File('somefile.mat','r')
+    data = f.get('data/variable1')
+    data = np.array(data) # For converting to a NumPy array
+-----------------------------------------------------------
+    import scipy.io
+    mat = scipy.io.loadmat('file.mat')
+    save('mat.mat', '-v7')
+-----------------------------------------------------------
+
+
 """
 import pandas as pd
 import numpy as np
 
-def load(file_name:str):
+def load(file_name:str, data_type=None):
     """Data loading
 
+
     :param file_name:   path to file with data
+    :param data_type:   dataset datatype (.mat, .csv, .txt, h5py, etc.)
     :return data:       loaded data
     """
     data = pd.read_csv(file_name,skipinitialspace=True)
--- data/data_description.md
+++ data/data_description.md
@@ -1,1 +1,7 @@
-## Data description
+# Data description
+
+## data_pneumatic_actuator
+Data from pneumatic actuator with different faults.
+
+## data_RAE
+Data from RAE aircraft acceleration measurements. (No fault) 
